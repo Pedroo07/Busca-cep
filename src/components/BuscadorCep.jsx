9b5805b07e78b906099f477e2deb03adc39911c8
Pedroo <pedrohenriquemoalive@gmail.com>
2023-12-20 10:03:46 -0300

    Adicionando função com a tecla Enter

--- src/components/BuscadorCep.jsx
+++ src/components/BuscadorCep.jsx
@@ -9,17 +9,23 @@ const BuscadorCep = () => {
         try{
             const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
             setEndereco(response.data)
+            console.log(response.data)
         }catch(error){
             console.log('Error ao buscar o Cep', error)
             setEndereco(null)
         }
+    }
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter'){
+        BuscarCep()    
+    }
     }
     
   return (
     <div className="container">
         <label className="input">
             CEP:
-            <input className="result-input" type="text" value={cep} onChange={(e) => setCep(e.target.value)} />
+            <input className="result-input" type="text" value={cep} onChange={(e) => setCep(e.target.value)} onKeyDown={handleKeyDown} />
         </label>
         <button className="button" onClick={BuscarCep}>Buscar CEP</button>
 
@@ -28,8 +34,8 @@ const BuscadorCep = () => {
                 <h2 className="result-title">Resultado:</h2>
                 <p className="result-item">CEP: {endereco.cep}</p>
                 <p className="result-item">DDD: {endereco.ddd}</p>
-                <p className="result-item">Cidade: {endereco.cidade}</p>
-                <p className="result-item">Bairro: {endereco.localidade}</p>
+                <p className="result-item">Cidade: {endereco.localidade}</p>
+                <p className="result-item">Bairro: {endereco.bairro}</p>
                 <p className="result-item">Estado: {endereco.uf}</p>
             </div>
         )}
