9400fca59f133d4a0ef95615768e409a8a546337
Pedroo <pedrohenriquemoalive@gmail.com>
2023-12-20 12:28:21 -0300

    config gh-pages

--- src/components/BuscadorCep.jsx
+++ src/components/BuscadorCep.jsx
@@ -6,42 +6,42 @@ const BuscadorCep = () => {
     const [cep, setCep] = useState('')
     const [endereco, setEndereco] = useState(null)
     const BuscarCep = async () => {
-        try{
+        try {
             const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
             setEndereco(response.data)
             console.log(response.data)
-        }catch(error){
+        } catch (error) {
             console.log('Error ao buscar o Cep', error)
             setEndereco(null)
         }
     }
     const handleKeyDown = (event) => {
-        if (event.key === 'Enter'){
-        BuscarCep()    
+        if (event.key === 'Enter') {
+            BuscarCep()
+        }
     }
-    }
-    
-  return (
-    <div className="container">
-        <label className="input">
-            CEP:
-            <input className="result-input" type="text" value={cep} onChange={(e) => setCep(e.target.value)} onKeyDown={handleKeyDown}/>
-        </label>
-        <button className="button" onClick={BuscarCep}>Buscar CEP</button>
 
-        {endereco && (
-            <div className="result-container">
-                <h2 className="result-title">Resultado:</h2>
-                <p className="result-item">CEP: {endereco.cep}</p>
-                <p className="result-item">DDD: {endereco.ddd}</p>
-                <p className="result-item">Cidade: {endereco.localidade}</p>
-                <p className="result-item">Bairro: {endereco.bairro}</p>
-                <p className="result-item">Estado: {endereco.uf}</p>
-            </div>
-        )}
-    </div>
-    
-  )
+    return (
+        <div className="container">
+            <label className="input">
+                CEP:
+                <input className="result-input" type="text" value={cep} onChange={(e) => setCep(e.target.value)} onKeyDown={handleKeyDown} />
+            </label>
+            <button className="button" onClick={BuscarCep}>Buscar CEP</button>
+
+            {endereco && (
+                <div className="result-container">
+                    <h2 className="result-title">Resultado:</h2>
+                    <p className="result-item">CEP: {endereco.cep}</p>
+                    <p className="result-item">DDD: {endereco.ddd}</p>
+                    <p className="result-item">Cidade: {endereco.localidade}</p>
+                    <p className="result-item">Bairro: {endereco.bairro}</p>
+                    <p className="result-item">Estado: {endereco.uf}</p>
+                </div>
+            )}
+        </div>
+
+    )
 }
 
 export default BuscadorCep
